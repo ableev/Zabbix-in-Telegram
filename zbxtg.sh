9ec4773d3e7f096cf1c37f11240b38db8f29d980
Ilya Ableev <ilya@ableev.com>
2015-12-09 21:06:00 +0300

    This fixes #8

--- zbxtg.sh
+++ zbxtg.sh
@@ -33,19 +33,22 @@ echo "${BODY}" | grep -q "${ZBX_TG_PREFIX};chat" && TG_CHAT=1
 
 if [ "${TG_CHAT}" -eq 1 ]
 then
-    MATCH=".chat.:{.id.:.*,.title.:.${TO}.}"
     TG_CONTACT_TYPE="chat"
 else
-    MATCH=".chat.:{.id.:.*,.username.:.${TO}.}"
     TG_CONTACT_TYPE="user"
 fi
 
-TG_CHAT_ID=$(cat ${TMP_UIDS} | grep -Po "^${TO};${TG_CONTACT_TYPE};\K.[0-9]+" | tail -1)
+TG_CHAT_ID=$(cat ${TMP_UIDS} | awk -F ';' '{if ($1 == "'${TO}'" && $2 == "'${TG_CONTACT_TYPE}'") print $3}' | tail -1)
 
 if [ -z "${TG_CHAT_ID}" ]
 then
     TG_UPDATES=$(${CURL_TG}/getUpdates)
-    TG_CHAT_ID=$(echo "${TG_UPDATES}" | grep -P "${MATCH}" | tail -1 | grep -Po 'chat....id..\K.[0-9]+')
+    if [ "${TG_CHAT}" -eq 1 ]
+    then
+        TG_CHAT_ID=$(echo "${TG_UPDATES}" | sed -e 's/["}{]//g' | awk -F ',' '{if ($8 == "type:group" && $7 == "title:'${TO}'") {gsub("chat:id:", "", $6); print $6}}' | tail -1)
+    else
+        TG_CHAT_ID=$(echo "${TG_UPDATES}" | sed -e 's/["}{]//g' | awk -F ',' '{if ($10 == "type:private" && $5 == "username:'${TO}'") {gsub("chat:id:", "", $6); print $6}}' | tail -1)
+    fi
     echo "${TO};${TG_CONTACT_TYPE};${TG_CHAT_ID}" >> ${TMP_UIDS}
 fi
 
@@ -60,9 +63,9 @@ ${TG_TEXT}" 2>/dev/null
 
     "image")
         PERIOD=3600 # default period
-        echo "${BODY}" | grep -q "^${ZBX_TG_PREFIX};graphs_period" && PERIOD=$(echo "${BODY}" | grep -Po "^${ZBX_TG_PREFIX};graphs_period=\K[0-9]+" || echo 3600)
-        ZBX_ITEMID=$(echo "${BODY}" | grep -Po "^${ZBX_TG_PREFIX};itemid:\K[0-9]+")
-        ZBX_TITLE=$(echo "${BODY}" | grep -Po "^${ZBX_TG_PREFIX};title:\K.*")
+        echo "${BODY}" | grep -q "^${ZBX_TG_PREFIX};graphs_period" && PERIOD=$(echo "${BODY}" | awk -F 'zbxtg;graphs_period=' '{if ($2 != "") print $2}' | tail -1 | grep -Eo '[0-9]+' || echo 3600)
+        ZBX_ITEMID=$(echo "${BODY}" | awk -F 'zbxtg;itemid:' '{if ($2 != "") print $2}' | tail -1 | grep -Eo '[0-9]+')
+        ZBX_TITLE=$(echo "${BODY}" | awk -F 'zbxtg;title:' '{if ($2 != "") print $2}' | tail -1)
         URL="${ZBX_SERVER}/chart3.php?period=${PERIOD}&name=${ZBX_TITLE}&width=900&height=200&graphtype=0&legend=1&items[0][itemid]=${ZBX_ITEMID}&items[0][sortorder]=0&items[0][drawtype]=5&items[0][color]=00CC00"
         login
         CACHE_IMAGE="${TMP_DIR}/graph.${ZBX_ITEMID}.png"
